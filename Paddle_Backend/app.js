738131f00923e6aef2241662f3ed0aa64391a1bd
Lucas Shearer <luke.t.shearer@gmail.com>
2020-03-19 14:19:48 -0700

    Updated to use actual Minecraft Data. Only small sample size for now but usable in front end.

--- Paddle_Backend/app.js
+++ Paddle_Backend/app.js
@@ -10,7 +10,7 @@ var logger = require('morgan');
 //import {buildSchema} from 'graphql';
 //
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users'); //users
 
 var app = express();
 
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter); ///users
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
